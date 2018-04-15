a22db45dd9f99f0d3b1032105099ea2722cacb20
Jenish Kanani <jenishkanani6@icloud.com>
2018-04-15 00:40:44 -0400

    Initial Commit

---
+++ CMSC389L/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CMSC389L
+//
+//  Created by Jenish Kanani on 4/15/18.
+//  Copyright © 2018 Jenish Kanani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
